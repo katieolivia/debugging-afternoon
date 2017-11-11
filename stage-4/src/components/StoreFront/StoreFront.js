65c58ce555e356602a9a34bcb41f6e3758b083b1
Kathryn Nutzman <katieonutzman@Kathryns-MacBook-Pro.local>
2017-11-11 12:13:14 -0700

    fixed one  more thing

--- stage-4/src/components/StoreFront/StoreFront.js
+++ stage-4/src/components/StoreFront/StoreFront.js
@@ -10,7 +10,7 @@ class StoreFront extends Component {
     }
 
     render() {
-        console.log(this.props.products);
+        console.log(1111111111, this.props.products);
         let productDisplay = this.props.products.map((element, index) => {
             return (
                 <div className="product-container" key={index}>
